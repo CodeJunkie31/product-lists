04694829a8c4c899d662195d3230e3da7bb643be
Michelle Dlamini <155561385+CodeJunkie31@users.noreply.github.com>
2026-02-20 17:40:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductList from "./ProductList";
+import SearchResults from "./SearchResults";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/search" element={<SearchResults />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
